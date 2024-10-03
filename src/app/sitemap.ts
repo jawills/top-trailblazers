010f13aad6bcfb58b76e4579278e2f98e4f15b74
Justin Wills <Justin1.wills@gmail.com>
2024-10-03 11:57:35 +0200

    added about page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { type MetadataRoute } from "next"
 import { siteConfig } from "~/config/site"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = [""].map((route) => ({
+  const routes = ["/", '/about'].map((route) => ({
     url: `${siteConfig.url}${route}`,
     lastModified: new Date().toISOString(),
   }))
